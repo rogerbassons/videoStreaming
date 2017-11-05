8ecea134c6b09a4541a791371f287faff6530ed9
Roger Bassons <rogerbassons@gmail.com>
2017-11-05 10:32:41 -0500

    fixed args

--- streamFile.sh
+++ streamFile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 #ffmpeg -i $1 -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 stream.m3u8
-ffmpeg -i $1 -map 0 -codec:v libx264 -codec:a aac -f ssegment -segment_list stream.m3u8 -segment_list_flags +live -segment_time 10 out%03d.ts
+#ffmpeg -i $1 -map 0 -codec:v libx264 -codec:a aac -f ssegment -segment_list stream.m3u8 -segment_list_flags +live -segment_time 10 out%03d.ts
+
+ffmpeg  -i $1 -c:v libx264 -c:a aac -pix_fmt yuv420p -f hls -hls_time 10 -hls_list_size 50 -hls_flags delete_segments stream.m3u8
