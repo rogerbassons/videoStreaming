c41de547f75ff4a24dbb730b6175f21e4441f23a
Roger Bassons <rogerbassons@gmail.com>
2017-11-21 21:39:58 -0500

    fix webcam hls args

--- stream.sh
+++ stream.sh
@@ -13,7 +13,10 @@ if [ ! -d "$DIR" ]; then
     mkdir stream
 fi
 
-[ "$1" == "webcam" ] && ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 -codec:v libx264 -codec:a aac -pix_fmt yuv420p -f hls -hls_time 10 -hls_list_size 3 -hls_wrap 4 -hls_flags delete_segments $DIR/stream.m3u8 && exit 0
+hlsTime=10
+hlsSize=50
 
-ffmpeg  -re -i $1 -c:v copy -c:a copy -f hls -hls_time 10 -hls_list_size 50 -hls_flags delete_segments $DIR/stream.m3u8
+[ "$1" == "webcam" ] && ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 -codec:v libx264 -codec:a aac -pix_fmt yuv420p -f hls -hls_time $hlsTime -hls_list_size $hlsSize -hls_flags delete_segments $DIR/stream.m3u8 && exit 0
+
+ffmpeg  -re -i $1 -c:v copy -c:a copy -f hls -hls_time $hlsTime -hls_list_size $hlsSize -hls_flags delete_segments $DIR/stream.m3u8
 
